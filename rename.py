d107473d52fdcef1a01d6a613eb63df68a1a829c
Anpy <ANoobyDev@gmail.com>
2022-02-20 15:28:34 +0100

    Modified 'rename.py' so it is easier to expand

--- rename.py
+++ rename.py
@@ -1,18 +1,30 @@
 import os
 
-xtn=input("What extension?")
+xtn = input("What extension?:\n")
 
 dir_name = '.'
 
 list_of_files = sorted( filter( lambda x: os.path.isfile(os.path.join(dir_name, x)),os.listdir(dir_name) ) )
 
-num1 = num2 = 0
+nums = []
+for i in range(len(str(len(list_of_files)))):
+    nums += [0]
 
 for file in list_of_files:
-    num1=num1+1
-    if num1==10:
-        num1=0
-        num2=num2 + 1
-    num = str(num2)+str(num1)
-    print(file)
-    os.system(f"mv '{file}' '{str(num)}'.{xtn}")
+    nums[-1] += 1
+    if nums[-1] == 10:
+        nums[-1] = 0
+        nums[-2] += 1
+        if nums[-2] == 10:
+            nums[-2] = 0
+            nums[-3] += 1
+            if nums[-3] == 10:
+                nums[-3] = 0
+                nums[-4] += 1
+
+    num = ""
+    for i in nums:
+        num = num + str(i)
+
+    print(file + f" --> {str(num)}.{xtn}") 
+    os.system(f"mv '{file}' '{str(num)}.{xtn}'")
